4a8eaf98b49681ff44f0a90c153117979dc279d1
Horváth Csongor <sifuto2013@gmail.com>
2025-05-13 16:11:47 +0200

    ADD| GaussinaMixtureSimulator

--- src/scisbi/simulator/__init__.py
+++ src/scisbi/simulator/__init__.py
@@ -2,7 +2,8 @@
 
 # Import the simulator classes to make them available under the scisbi.simulator namespace
 from .GaussianSimulator import GaussianSimulator
+from .GaussianMixtureSimulator import GaussianMixtureSimulator
 
 
 # Define __all__ for the base submodule
-__all__ = [GaussianSimulator]
+__all__ = [GaussianSimulator, GaussianMixtureSimulator]
