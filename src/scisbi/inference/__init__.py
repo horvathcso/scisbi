de7dda2bcc08f8396c20baab18b535a09f9399ca
Horváth Csongor <sifuto2013@gmail.com>
2025-07-14 16:02:37 +0200

    UPDATE| init

--- src/scisbi/inference/__init__.py
+++ src/scisbi/inference/__init__.py
@@ -1,6 +1,15 @@
 # scisbi/inference/__init__.py
 
 # Import the inference classes to make them available under the scisbi.inference namespace
+from .ABC import ABCRejectionSampling, ABCPosterior
+from .ABCMCMC import ABCMCMC, ABCMCMCPosterior
+from .ABCSMC import ABCSMC, ABCSMCPosterior
 
-# Define __all__ for the base submodule
-__all__ = []
+__all__ = [
+    "ABCRejectionSampling",
+    "ABCPosterior",
+    "ABCMCMC",
+    "ABCMCMCPosterior",
+    "ABCSMC",
+    "ABCSMCPosterior",
+]
