46897c26b5eeb7364521eba608413f0f398b6837
Horváth Csongor <sifuto2013@gmail.com>
2025-07-15 16:08:14 +0200

    UPDATE|init

--- src/scisbi/inference/__init__.py
+++ src/scisbi/inference/__init__.py
@@ -5,6 +5,17 @@ from .ABC import ABCRejectionSampling, ABCPosterior
 from .ABCMCMC import ABCMCMC, ABCMCMCPosterior
 from .ABCSMC import ABCSMC, ABCSMCPosterior
 
+# Try to import neural network based methods (require torch)
+try:
+    from .JANA import JANA, JANAPosterior
+    from .Simformer import Simformer, SimformerPosterior, SimformerTransformer
+
+    NEURAL_METHODS_AVAILABLE = True
+except ImportError:
+    # If torch is not available, these methods won't be available
+    NEURAL_METHODS_AVAILABLE = False
+
+
 __all__ = [
     "ABCRejectionSampling",
     "ABCPosterior",
@@ -13,3 +24,15 @@ __all__ = [
     "ABCSMC",
     "ABCSMCPosterior",
 ]
+
+# Add neural methods to __all__ if available
+if NEURAL_METHODS_AVAILABLE:
+    __all__.extend(
+        [
+            "JANA",
+            "JANAPosterior",
+            "Simformer",
+            "SimformerPosterior",
+            "SimformerTransformer",
+        ]
+    )
